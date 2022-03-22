f59d35fb9ac290c4cab35db5d02d6eea6e9ef9ed
John Cornforth <jcornforth@johns-mbp.home>
2022-03-22 10:34:21 +0000

    added more cli

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Executing mgen.cli"
 $JBOSS_HOME/bin/jboss-cli.sh -c --file=$JBOSS_HOME/extensions/mgen.cli
+$JBOSS_HOME/bin/jboss-cli.sh -c --file=$JBOSS_HOME/customscript/datasource_postgres.cli
